1e7886e5e1d026d5c11bd23fbef6f34a3ba759bf
Robb P. Matzke <matzke1@llnl.gov>
2017-12-14 19:23:42 -0500

    Added nVidia CUDA support to spock
    These changes are supported by the spock tools, but not
    yet by the "rmc" script (that's coming next).
    
    * Added nVidia CUDA compilers
      Definitions:
        + "cuda" is a base language (like "c", "c++", "fortran")
        + "cuda" is also a language (like "c++03", "c++11", etc.)
        + "nvidia" is a vendor (like "gnu", "llvm", "intel")
    
      Packages (see "spock-ls --help"):
        + nvidia-compilers: any and all compilers from nVidia
        + nvidia-system-compilers: any and all system-installed compilers from nVidia
        + nvidia-cuda: specific CUDA compilers provided by nVidia (i.e., the "nvcc" command)
        + cuda-compiler: all CUDA compilers from any vendor
    
      The CUDA packages use "nvcc" as the compiler name rather
      than using something more vendor-agnostic. That's probably
      fine for now since the only CUDA compiler is from nVidia.
      This is in contrast to C, C++, and Fortran compilers to which
      Spock gives generic names "cc", "c++", and "fc".
    
    * Renamed "raja" package
        + "raja" is now a virtual package (see NOTICE below)
    
        + "raja-cuda" is raja configured with cuda support and
           satisfies "raja" dependency.
    
        + "raja-nocuda" is raja configured without cuda support
          and satisifies "raja" dependency.
    
    NOTICE NOTICE NOTICE:
       You should do "spock-rm -f raja" before installing this version.
    
       I did it this way rather than bumping the spock version number
       so that you don't have to re-install all the packages.
    
       The "raja-cuda" package does not build correctly yet because
       I haven't taken the time to figure out how to specify the
       CUDA compiler.

--- scripts/impl/compiler-support.sh
+++ scripts/impl/compiler-support.sh
@@ -25,6 +25,7 @@ spock-compiler-base-language-var() {
         c) echo "C" ;;
         fortran) echo "FORTRAN" ;;
         java) echo "JAVA" ;;
+	cuda) echo "CUDA" ;;
         *) die "unknown base language: $base_language" ;;
     esac
 }
@@ -128,14 +129,18 @@ spock-compiler-install() {
 
                 spock-compiler-onoff x                  x     fc
                 ;;
+
+	    cuda)
+		spock-compiler-onoff "$compiler_vendor" nvidia nvcc
         esac
     ) || exit 1
 
     local compiler_cmd=
     case "$compiler_baselang" in
-        c++)     compiler_cmd=c++ ;;
-        c)       compiler_cmd=cc  ;;
-        fortran) compiler_cmd=fc  ;;
+        c++)     compiler_cmd=c++  ;;
+        c)       compiler_cmd=cc   ;;
+        fortran) compiler_cmd=fc   ;;
+	cuda)    compiler_cmd=nvcc ;;
         *)       die "baselang $compiler_baselang not supported yet (spock-compiler-install)"
     esac
 
@@ -394,6 +399,10 @@ spock-compiler-install-program() {
             spock-compiler-conditional-install-language "$collection_spec" c ""     "$exe" -std=gnu11
             spock-compiler-conditional-install-language "$collection_spec" c "-m32" "$exe" -std=gnu11
             ;;
+
+	nvidia:cuda)
+	    spock-compiler-conditional-install-language "$collection_spec" cuda "" "$exe"
+	    ;;
 
         *:c++)
             spock-compiler-conditional-install-language "$collection_spec" c++ ""     "$exe"
