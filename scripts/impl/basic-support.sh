dfccba6540a7a499756ada25730f73642da08e77
Robb Matzke <matzke@llnl.gov>
2021-07-13 10:59:30 -0400

    tempfile command is deprecated in Ubuntu-21.04

--- scripts/impl/basic-support.sh
+++ scripts/impl/basic-support.sh
@@ -5,14 +5,20 @@ die() {
     exit 1
 }
 
-# The tempfile command is not always available, so reimplement it here if necessary.
+# The 'tempfile' command is not always available, so reimplement it here if necessary.
+# Now 'tempfile' is deprecated, so use mktemp if available.
 tempfile() {
-    local real_tempfile="$(which tempfile 2>/dev/null)"
-    if [ -n "$real_tempfile" ]; then
-	"$real_tempfile"
+    local real_mktemp="$(which mktemp 2>/dev/null)"
+    if [ -n "$real_mktemp" ]; then
+	"$real_mktemp"
     else
-	local tmpdir="${TMPDIR:-$TEMPDIR}"
-	mkdir -p "${tmpdir:-/tmp}"
-	echo "${tmpdir:-/tmp}/spock-$RANDOM$RANDOM$RANDOM"
+	local real_tempfile="$(which tempfile 2>/dev/null)"
+	if [ -n "$real_tempfile" ]; then
+	    "$real_tempfile"
+	else
+	    local tmpdir="${TMPDIR:-$TEMPDIR}"
+	    mkdir -p "${tmpdir:-/tmp}"
+	    echo "${tmpdir:-/tmp}/spock-$RANDOM$RANDOM$RANDOM"
+	fi
     fi
 }
